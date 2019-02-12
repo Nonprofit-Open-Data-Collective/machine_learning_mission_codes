fbc2442bbb847321cda1dc28b869b40b868498fe
Francisco Santamarina <fjsantam@gmail.com>
2019-02-12 10:09:53 -0800

    Test upload of R script for SVM

---
+++ docs/SVM Sample Code.R
@@ -0,0 +1,7 @@
+# SVM Sample cases
+
+## Source article: https://journal.r-project.org/archive/2013/RJ-2013-001/RJ-2013-001.pdf
+## Data is a document-term matrix 
+
+doc_matrix <- read.csv( "~/DATA/Mission w Corpus and DFM.csv")
+
